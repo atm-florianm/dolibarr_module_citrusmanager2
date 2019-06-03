3c5b9e5fe98b91e84f7abb1b9a4641462159b553
atm-florian.m <florian.mortgat@atm-consulting.fr>
2019-06-03 12:13:08 +0200

    (fix) create preset categories using "INSERT IGNORE" (with hard-coded row IDs) to prevent the insertion of duplicates

--- sql/data_llx_c_citrusmanager2_category.sql
+++ sql/data_llx_c_citrusmanager2_category.sql
@@ -1,5 +1,6 @@
-INSERT INTO llx_c_citrusmanager2_category (code, label, active) VALUES
-    ("sour", "Agrume acide", 1),
-    ("bitter", "Agrume amer", 1),
-    ("sweet", "Agrume doux", 1),
-    ("extra-sweet", "Agrume particulièrement sucré", 1);
+
+INSERT IGNORE INTO llx_c_citrusmanager2_category (rowid, code, label, active) VALUES
+    (1, "sour", "Agrume acide", 1),
+    (2, "bitter", "Agrume amer", 1),
+    (3, "sweet", "Agrume doux", 1),
+    (4, "extra-sweet", "Agrume particulièrement sucré", 1);
